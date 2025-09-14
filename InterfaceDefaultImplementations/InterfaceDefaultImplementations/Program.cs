143888bb3ddb2dcbb7e508f37146b4be067f2216
RafaelJCamara <rafaelcamarac@gmail.com>
2025-09-14 16:12:40 +0200

    Add source code for default implementations in interfaces.

---
+++ InterfaceDefaultImplementations/InterfaceDefaultImplementations/Program.cs
@@ -0,0 +1,38 @@
+﻿public interface IInterface
+{
+    private static int a = 5;
+
+    void MethodA();
+    void MethodB()
+    {
+        Console.WriteLine($"Default implementation of MethodB in Interface. Value of a: {a}");
+        PrivateMethod();
+    }
+
+    private static void PrivateMethod()
+    {
+        Console.WriteLine("This is a private method in the interface.");
+    }
+}
+
+public class ClassA : IInterface
+{
+    public void MethodA()
+    {
+        throw new NotImplementedException();
+    }
+
+    //public void MethodB()
+    //{
+    //    Console.WriteLine("ClassA's implementation of MethodB.");
+    //}
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IInterface instance = new ClassA();
+        instance.MethodB();
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # YT-Interface-Default-Implementations
+Watch this example live [here](https://youtu.be/ORs_U2dZeWM).
